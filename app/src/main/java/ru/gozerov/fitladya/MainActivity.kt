d923b3f96aeb00767b7ac889e8403910056b317e
Grigory <ozeroffgrisha@gmail.com>
2024-06-03 18:40:06 +0300

    init commit

---
+++ app/src/main/java/ru/gozerov/fitladya/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.gozerov.fitladya
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.vk.api.sdk.utils.VKUtils.getCertificateFingerprint
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val fingerprints = getCertificateFingerprint(this, this.packageName)
+        fingerprints?.forEach {
+            Log.e("AAAA", it.toString())
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
